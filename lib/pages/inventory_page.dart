9af9ff887fdb95a9d2aede25a59bc4636606ab69
Daniel Józefiuk <daniel.jozefiuk@gmail.com>
2023-09-18 22:26:49 +0200

    .

--- lib/pages/inventory_page.dart
+++ lib/pages/inventory_page.dart
@@ -2,6 +2,7 @@ import 'package:fluent_ui/fluent_ui.dart';
 import 'package:home_manager/models/product.dart';
 import 'package:home_manager/services/inventory_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:uuid/uuid.dart';
 
 class InventoryPage extends StatefulWidget {
   const InventoryPage({super.key});
@@ -16,9 +17,14 @@ class _InventoryPageState extends State<InventoryPage> {
     return Column(
       children: [
         Center(
-          child: FilledButton(
-            child: const Icon(FluentIcons.add, size: 24.0),
-            onPressed: () => provider.addProduct(Product()),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: FilledButton(
+              child: const Icon(FluentIcons.add, size: 24.0),
+              onPressed: () => provider.addProduct(Product(
+                id: const Uuid().v4(),
+              )),
+            ),
           ),
         ),
         Expanded(
@@ -31,13 +37,19 @@ class _InventoryPageState extends State<InventoryPage> {
                 subtitle: Text(product.price.toStringAsFixed(2)),
                 trailing: Row(
                   children: [
-                    FilledButton(
-                      onPressed: () {},
-                      child: const Icon(FluentIcons.edit),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: FilledButton(
+                        onPressed: () {},
+                        child: const Icon(FluentIcons.edit),
+                      ),
                     ),
-                    FilledButton(
-                      onPressed: () => provider.deleteProduct(index),
-                      child: const Icon(FluentIcons.delete),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: FilledButton(
+                        onPressed: () => provider.deleteProduct(index),
+                        child: const Icon(FluentIcons.delete),
+                      ),
                     ),
                   ],
                 ),
